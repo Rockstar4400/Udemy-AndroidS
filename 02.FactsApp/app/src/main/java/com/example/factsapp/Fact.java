fcac625f219af0047fb4ea0d791759b7190630bd
Alberto Del Angel <rockstar4400@gmail.com>
2022-08-13 10:15:06 -0500

    App List

---
+++ 02.FactsApp/app/src/main/java/com/example/factsapp/Fact.java
@@ -0,0 +1,14 @@
+package com.example.factsapp;
+
+public class Fact {
+    public static String[] getFacts(){
+
+        return new String[]{
+           "Animals that",
+           "A honey bee",
+           "There is",
+           "Babies",
+           "Alone"
+        };
+    }
+}
